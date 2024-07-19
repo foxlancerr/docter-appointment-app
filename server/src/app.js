0957ce214d0ab10505bf63c8a278cda84fe38746
M-Asimm <kpkprince55persia@gmail.com>
2024-07-20 01:47:32 +0500

    api/appointment are tested correctly and working properly

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,7 @@ import doctorRouter from "./routes/doctorRouter.js";
 import patientRouter from "./routes/patientRouter.js";
 import adminRouter from "./routes/adminRouter.js";
 import notificationRouter from "./routes/notificationRouter.js";
+import appointmentRouter from "./routes/appointmentRouter.js";
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 //routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/doctor", doctorRouter);
+app.use("/api/v1/appointments", appointmentRouter);
 // app.use("/api/v1/admins", adminRouter);
 app.use("/api/v1/patients", patientRouter);
 app.use("/api/v1/notification", notificationRouter);
@@ -22,8 +24,5 @@ app.use("/api/v1/notification", notificationRouter);
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
-
-
-
 
 export { app };
